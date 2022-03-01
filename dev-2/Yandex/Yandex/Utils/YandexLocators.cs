d8a9b6674cb4a8c226ceadcabb66a2c1c7ff04fb
Vladislav Luksha <VladislavLuksha@coherentsolutions.com>
2022-03-01 13:46:54 +0300

    feat: delete utils files

--- dev-2/Yandex/Yandex/Utils/YandexLocators.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenQA.Selenium;
-
-namespace Yandex.Utils
-{
-    public class YandexLocators
-    {
-        public By loginButton = By.XPath("//a[@class = 'home-link desk-notif-card__login-new-item desk-notif-card__login-new-item_enter home-link_black_yes home-link_hover_inherit']");
-    }
-}
